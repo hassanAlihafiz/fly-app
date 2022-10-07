6ac0af870cbc6705fd68d241ef5f497f21717058
hassanAlihafiz <bali37088@gmail.com>
2022-10-07 14:27:09 +0500

    Hassan

---
+++ navigation/StackNavigation.js
@@ -0,0 +1,23 @@
+import { StackRouter } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LandingScreen from "../screens/LandingScreen";
+import LoginScreen from "../screens/LoginScreen";
+import DrawerTab from "./DrawerNavigation";
+
+const Stack = createNativeStackNavigator();
+
+const StackNavigation = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name="Landing" component={LandingScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} />
+
+      <Stack.Screen name="HomeScreen" component={DrawerTab} />
+    </Stack.Navigator>
+  );
+};
+export default StackNavigation;
