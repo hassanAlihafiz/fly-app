342702ca362dc1d294afbf3a4d36a190f3dde60c
Razi Hassan <raziulh@outlook.com>
2022-11-07 09:53:05 +0500

    map

--- components/PickDropMap.js
+++ components/PickDropMap.js
@@ -8,6 +8,7 @@ import {
   Dimensions,
   Platform,
   TouchableOpacity,
+  TextInput,
 } from "react-native";
 import MapView, {
   Marker,
@@ -18,28 +19,53 @@ import {
   heightPercentageToDP,
   widthPercentageToDP,
 } from "react-native-responsive-screen";
-import { Entypo } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 const PickDropMap = () => {
   const navigation = useNavigation();
+  const [maploading, setMaploading] = React.useState();
   const [locationState, setLocationState] = React.useState({
     latitude: null,
     longitude: null,
     latitudeDelta: null,
     longitudeDelta: null,
   });
-  console.log(locationState);
+
   return (
     <View>
+      <MapView
+        loadingEnable={maploading}
+        provider={PROVIDER_GOOGLE}
+        style={{ height: "100%", width: "100%" }}
+        userInterfaceStyle="dark"
+        onRegionChange={(e) => setLocationState(e)}
+      >
+        <View
+          pointerEvents="none"
+          style={{
+            position: "absolute",
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "transparent",
+          }}
+        >
+          <FontAwesome5 name="map-marker-alt" size={30} color="red" />
+        </View>
+      </MapView>
+
       <View
         style={{
           display: "flex",
-          margin: 10,
+          margin: 20,
           padding: 10,
           borderRadius: 10,
           position: "absolute",
           backgroundColor: "white",
-          width: "95%",
+          width: "90%",
           zIndex: 2,
         }}
       >
@@ -60,33 +86,52 @@ const PickDropMap = () => {
               fontWeight: "bold",
             }}
           >
-            Packages
+            Pickup Location
           </Text>
           <MaterialIcons name="local-car-wash" size={24} color="black" />
         </View>
-      </View>
-      <MapView
-        provider={PROVIDER_GOOGLE}
-        style={{ marginTop: 10, height: "100%", width: "100%" }}
-        userInterfaceStyle="dark"
-        onRegionChange={(e) => setLocationState(e)}
-      >
-        <View
-          pointerEvents="none"
+        <TextInput
+          placeholder="Enter pickup location"
+          autoComplete="name"
+          placeholderTextColor="#9EA0A4"
+          // onChangeText={(e) => handleData("firstName", e)}
           style={{
-            position: "absolute",
-            top: 0,
-            bottom: 0,
-            left: 0,
-            right: 0,
-            alignItems: "center",
-            justifyContent: "center",
-            backgroundColor: "transparent",
+            height: 40,
+            borderWidth: 1,
+            borderColor: "#d7d7d7",
+            paddingLeft: 10,
+            paddingRight: 10,
+            backgroundColor: "white",
+            borderRadius: 5,
           }}
-        >
-          <Entypo name="location-pin" size={24} color="black" />
-        </View>
-      </MapView>
+        />
+      </View>
+      <TouchableOpacity
+        style={{
+          position: "absolute",
+          bottom: 0,
+          height: 50,
+          marginVertical: 20,
+          marginHorizontal: 20,
+
+          backgroundColor: "#43ce51",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "90%",
+
+          borderRadius: 10,
+        }}
+        onPress={() =>
+          navigation.navigate("SelectDriver", {
+            // packageData: packageData,
+            // imgUrl: require("../assets/images.jpg"),
+          })
+        }
+      >
+        <Text style={{ color: "white", alignSelf: "center" }}>
+          Confirm pickup
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
